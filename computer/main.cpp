bb920c43f52ca2834c8b08c6ba0dcbf0a5021871
Ivan Manan <ivanmanan@gmail.com>
2019-01-07 14:56:30 -0800

    Laid down infrastructure for makefile

--- computer/main.cpp
+++ computer/main.cpp
@@ -1,5 +1,11 @@
 /*
-Main code that goes onto the computer
+    Main code that goes onto the computer
+    Execute this to run trials from the LCDK output
+    This code would be running in background continuously, detecting changes in lcdk/output
+    It reads the text file made from train.cpp and apply HMM's onto the MFCC's from the LCDK
+    This file exports speech recognition and an appropriate response as output/test.txt
+
+    NOTE: I need to implement class objects and place it onto the MakeFile as well
 */
 
 #include <iostream>
---
+++ computer/train.cpp
@@ -0,0 +1,12 @@
+/*
+    Code that trains data set
+    Can implement some form of multithreading to process everything faster
+    This program produces a text file with all the data set
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello world!" << endl;
+}
