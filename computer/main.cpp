af00c0b9175838b6761ba40eb66071ad7486d16f
Ivan Manan <ivanmanan@gmail.com>
2019-01-08 14:07:39 -0800

    Edited makefile

--- computer/main.cpp
+++ computer/main.cpp
@@ -6,11 +6,32 @@
     This file exports speech recognition and an appropriate response as output/test.txt
 
     NOTE: I need to implement class objects and place it onto the MakeFile as well
+    NOTE: I need to make the C++ file paths and the Makefile compatible with Windows Powershell
 */
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
+vector<int> readTrainingFile() {
+    vector<int> temp;
+    return temp;
+}
+
+vector<int> readLCDK() {
+    vector<int> temp;
+    cout << "HI!"<< endl;
+    return temp;
+}
+
 int main() {
+
+    // Read text file
+    // I may want to make a vector of class objects
+    // I want to make variables here I guess?
+    // Can pass paramter and return vectors
+    vector<int> training_set = readTrainingFile();
+    vector<int> trial_set = readLCDK();
+
     cout << "Hello world!" << endl;
 }
--- computer/train.cpp
+++ computer/train.cpp
@@ -2,6 +2,9 @@
     Code that trains data set
     Can implement some form of multithreading to process everything faster
     This program produces a text file with all the data set
+
+    NOTE: I need to implement class objects and place it onto the MakeFile as well
+    NOTE: Need to read audio files; can store a text file with list of wav file names
 */
 
 #include <iostream>
