ee7adea3844479e1f4dba5ff529353ecae2446d1
Ivan Manan <ivanmanan@gmail.com>
2019-01-28 11:09:08 -0800

    Need erik to work

--- computer/train.cpp
+++ computer/train.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <string>
-#include "Chatbot.h"
+//#include "chatbot.h"
+
+/*
+    Used to test chatbot
+    May create separate file for chatbot
+    May use this for neural network if necessary
+*/
 
 using namespace std;
 
 int main() {
-	Chatbot Tad;    // Create a learner object
+/* 	Chatbot Tad;    // Create a learner object
 
 	cout << "Tad: Hi, my name is Tad, how can I help you today?";
-	/*
-		The following is the main loop. It will continue until the application is closed.
-		The user enters their input, and then the learner will respond.
-	*/
+		//The following is the main loop. It will continue until the application is closed.
+		//The user enters their input, and then the learner will respond.
+
 	for (;;) {
 		cout << "\nUser: ";    // User prompt
 		string phrase;
@@ -19,5 +24,5 @@ int main() {
 
 		cout << "Tad: ";
 		Tad.respond(phrase);    // Pass the user input to the learner and see if he can respond
-	}
+	} */
 }
