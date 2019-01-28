6c9581d275f83b24c48c4095baea6387ec4de3b3
Ivan Manan <ivanmanan@gmail.com>
2019-01-28 11:45:34 -0800

    Added chat.cpp

---
+++ computer/chat.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+//#include "chatbot.h"
+
+/*
+	Given an input, identify key words and output an appropriate response
+*/
+
+using namespace std;
+
+int main() {
+	cout << "Hello!" << endl;
+/* 	Chatbot Tad;    // Create a learner object
+
+	cout << "Tad: Hi, my name is Tad, how can I help you today?";
+		//The following is the main loop. It will continue until the application is closed.
+		//The user enters their input, and then the learner will respond.
+
+		// Pass string here that breaks out of the infinite loop
+
+	for (;;) {
+		cout << "\nUser: ";    // User prompt
+		string phrase;
+		getline(cin, phrase);    // Using getline for multi word input, then store in phrase. 
+
+		cout << "Tad: ";
+		Tad.respond(phrase);    // Pass the user input to the learner and see if he can respond
+	} */
+}
--- computer/train.cpp
+++ computer/train.cpp
@@ -1,28 +1,72 @@
 #include <iostream>
 #include <string>
-//#include "chatbot.h"
-
+#include <chrono>
+#include <thread>
+#include "./auxiliary/duplex.h"
 /*
-    Used to test chatbot
-    May create separate file for chatbot
-    May use this for neural network if necessary
+    Used to test HMM's and potentially neural network
+    TODO: Feed a direct line to the lcdk by copying from main.cpp on reading and writing text files
+    TODO: This should only be used for the computer program to print out identified words
+          Hence, there is no chatbot involved here
 */
 
 using namespace std;
+using namespace this_thread; // sleep_for, sleep_until
+using namespace chrono; // nanoseconds, system_clock, seconds
+
+
+void readLCDK() {
+
+    /*
+    while(1) {
+        // Read lcdk/start.txt if user triggers a therapy session
+        bool previousStart = readCharFile(START_FILE, false);
+        bool currentStart = readCharFile(START_FILE, false);
+        while(1) {
+            currentStart = readCharFile(START_FILE, currentStart);
+            if (currentStart != previousStart) {
+                break;
+            }
+            // NOTE: Read file every 2 second intervals
+            // NOTE: Use delay only for printing text; remove delay for final project
+            sleep_for(seconds(1));
+        }
+
+        cout << "\nPROGRAM: User initialized therapy session..." << endl;
+
+        // Initialize the computer/output/output.txt file
+        startOutputFile(COMPUTER_OUTPUT_FILE);
+
+        cout << "PROGRAM: Tad sent out the first message." << endl;
+
+        // NOTE: Variables used to detect changes made by the LCDK
+        bool previousChange = readCharFile(DONE_FILE, false);
+        bool currentChange = readCharFile(DONE_FILE, false);
+        while(1) {
+            
+            currentChange = readCharFile(DONE_FILE, currentChange);
+
+            if (currentChange != previousChange) {
+                previousChange = currentChange;
+                cout << "PROGRAM: LCDK created new inputs." << endl;
+                // Read new neural network inputs
+                bool finishConversation = conversate(INPUT_FILE, COMPUTER_BUSY_FILE);
+                // NOTE: When conversation is over, break from while-loop
+                //       and restart conversation again
+                if (finishConversation) {
+                    break;
+                }
+            }
+            // NOTE: Read file every 2 second intervals
+            // NOTE: Use delay only for printing text; remove delay for final project
+            sleep_for(seconds(1));
+        }
+        cout << "PROGRAM: Conversation finished. Listening for new conversation..." << endl;
+    }
+    */
+}
 
 int main() {
-/* 	Chatbot Tad;    // Create a learner object
-
-	cout << "Tad: Hi, my name is Tad, how can I help you today?";
-		//The following is the main loop. It will continue until the application is closed.
-		//The user enters their input, and then the learner will respond.
-
-	for (;;) {
-		cout << "\nUser: ";    // User prompt
-		string phrase;
-		getline(cin, phrase);    // Using getline for multi word input, then store in phrase. 
-
-		cout << "Tad: ";
-		Tad.respond(phrase);    // Pass the user input to the learner and see if he can respond
-	} */
+    // Comment this function out if you do not want to read from the LCDK
+    readLCDK();
 }
